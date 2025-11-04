09484608014328f26304880af2ae4a40e986ae81
Maksim Aksh <yaposhka277@gmail.com>
2025-11-04 16:45:15 +0300

    Update TeacherControllerIT

---
+++ src/test/java/ru/aksh/learningmanagement/controller/AbstractTestIT.java
@@ -0,0 +1,30 @@
+package ru.aksh.learningmanagement.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AbstractTestIT {
+
+    protected static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:18.0")
+            .withDatabaseName("integration-tests-db")
+            .withUsername("test")
+            .withPassword("test");
+
+    static {
+        postgreSQLContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+}
--- src/test/java/ru/aksh/learningmanagement/controller/TeacherControllerIT.java
+++ src/test/java/ru/aksh/learningmanagement/controller/TeacherControllerIT.java
@@ -3,16 +3,10 @@ package ru.aksh.learningmanagement.controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ActiveProfiles;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import ru.aksh.learningmanagement.domain.Teacher;
 import ru.aksh.learningmanagement.repository.TeacherRepository;
 
@@ -20,23 +14,12 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-
-@Testcontainers
-@ActiveProfiles("test")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class TeacherControllerIT {
+class TeacherControllerIT extends AbstractTestIT{
     @Autowired
     private TeacherRepository teacherRepository;
 
     @Autowired
     private TestRestTemplate restTemplate;
-
-    @Container
-    @ServiceConnection
-    private static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:18.0")
-            .withDatabaseName("integration-tests-db")
-            .withUsername("test")
-            .withPassword("test");
 
     @BeforeEach
     void resetDb() {
@@ -47,8 +30,7 @@ class TeacherControllerIT {
     void createTeacher_shouldReturnStatus200_ifValidRequest() {
         Teacher teacher = teacherRepository.save(new Teacher("Иван", "Сидоров"));
 
-        ResponseEntity<Teacher> response = restTemplate.postForEntity(
-                "/api/v1/teachers/create", teacher, Teacher.class);
+        ResponseEntity<Teacher> response = getResponseTeacherEntity(teacher);
 
         assertThat(response.getStatusCode(), is(HttpStatus.OK));
         assertThat(response.getBody().getId(), notNullValue());
@@ -70,8 +52,7 @@ class TeacherControllerIT {
     void createTeacher_shouldReturnStatus400_ifRequestDataIsEmpty() {
         Teacher teacher = teacherRepository.save(new Teacher("", ""));
 
-        ResponseEntity<Teacher> response = restTemplate.postForEntity(
-                "/api/v1/teachers/create", teacher, Teacher.class);
+        ResponseEntity<Teacher> response = getResponseTeacherEntity(teacher);
 
         assertThat(response.getStatusCode(), is(HttpStatus.BAD_REQUEST));
     }
@@ -80,9 +61,12 @@ class TeacherControllerIT {
     void createTeacher_shouldReturnStatus400_ifRequestDataIsInvalidSize() {
         Teacher teacher = teacherRepository.save(new Teacher("A", "B"));
 
-        ResponseEntity<Teacher> response = restTemplate.postForEntity(
-                "/api/v1/teachers/create", teacher, Teacher.class);
+        ResponseEntity<Teacher> response = getResponseTeacherEntity(teacher);
 
         assertThat(response.getStatusCode(), is(HttpStatus.BAD_REQUEST));
+    }
+
+    private ResponseEntity<Teacher> getResponseTeacherEntity(Teacher teacher) {
+        return restTemplate.postForEntity("/api/v1/teachers/create", teacher, Teacher.class);
     }
 }
